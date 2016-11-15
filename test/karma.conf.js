8050c17b1b06a37cd715933ec9aaa3ddf9636f49
mikeyjcat <mike@hewitson.co.za>
2016-11-15 05:41:14 +0200

    feat: added table for main page

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,7 +33,6 @@ module.exports = function(config) {
             'bower_components/angular-touch/angular-touch.js',
             'bower_components/angular-google-chart/ng-google-chart.js',
             'bower_components/angular-ui-router/release/angular-ui-router.js',
-            'bower_components/numeral/numeral.js',
             'bower_components/angular-mocks/angular-mocks.js',
             // endbower
             'app/scripts/**/*.js',
